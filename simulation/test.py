cc712e224ff6ba21b9ba83c87e388960b863e47e
Funaizhang <naifuzhang@gmail.com>
2019-11-25 12:21:32 +0800

    correct overlap algo mistakes

--- simulation/test.py
+++ simulation/test.py
@@ -2,7 +2,7 @@
 generate training data for the imitation learning
 '''
 import os
-import enviroment
+import enviroment_test
 from generate_questions import Qusetion
 from generate_actions import best_action
 import h5py
@@ -18,9 +18,13 @@ for scene in scene_list:
     my_action = best_action(my_enviroment)
 '''
 scene = "test-10-obj-01.txt"
-my_enviroment = enviroment.Environment(is_testing=1,testing_file = scene)
-ret = my_enviroment.ur5.get_obj_positions_and_orientations()
-print(ret)
+my_enviroment = enviroment_test.Environment(is_testing=1,testing_file = scene)
+# ret = my_enviroment.ur5.get_obj_positions_and_orientations()
+# print(ret)
+ret1 = my_enviroment.ur5.get_obj_positions_and_orientations_1()
+# print(ret1)
+overlaps1 = my_enviroment.ur5.check_overlap_1(2, 0.25)
+print(overlaps1)
 
 
                  
